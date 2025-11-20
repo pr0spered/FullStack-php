6285e7a94169f40e7b8013d8aca1eab27a9d8837
pr0spered <karthik.sujath@gmail.com>
2025-11-20 11:17:43 +0530

    Added index page

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Box Office</title>
+</head>
+
+<body>
+    <?php
+    include "db.php";
+    session_start();
+    $username = @$_SESSION["username"]
+    ?>
+
+    <div id = "header">
+        <div id="logo">
+            <a href="index.php">BoxOffice</a>
+        </div>
+
+        <div id="login">
+            <?php 
+            if (isset($_SESSION["username"])) {
+                echo '  <a>'.htmlspecialchars($username).'</a>
+                        <div id="login-drop">
+                            <a>Bookings</a>
+                            <a>Logout</a>
+                        </div>';
+
+            }
+            else {
+                echo '<a>Login</a>';
+            }
+            ?>
+        </div>
+    </div>
+
+    <div id="main">
+        <?php 
+        $sql = "SELECT name, poster FROM movies";
+        if ($result = mysqli_query($conn,$sql)) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<div class="movie-home">
+                    <img src="'.$row["poster"].'">
+                    <div class="movie-home-name">'.$row["name"].'</div>
+                    </div>';
+            }
+        }
+        else {
+            echo '<p>Error: Cannot Execute Query</p>';
+        }
+        mysqli_close($conn);
+        ?>
+    </div>
+</body>
+
